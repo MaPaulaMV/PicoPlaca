0280a69910e59d8d732264d3ee6ea688e565dba2
MaPaulaMV <ma.paulamonteros@gmail.com>
2021-02-19 15:53:58 -0500

    Layouts page master

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('title','Pico&Placa')
+@section('content')
+<div class="row">
+    <div class="card">
+        <div class="body">
+            <p>INDEX</p>
+        </div>
+    </div>
+</div>
+@endsection
+
